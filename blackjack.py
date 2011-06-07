c34534cf5c7cef70c216da58b0221f912853abf0
Tom Trebat <tjtrebat@gmail.com>
2011-06-07 15:06:31 -0400

    created images in __main__ to avoid garbage collection

--- blackjack.py
+++ blackjack.py
@@ -7,9 +7,8 @@ class Card:
     def __init__(self, rank, suit):
         self.rank = rank
         self.suit = suit
-        self.image = self.get_card_image()
-        
-    def get_card_image(self):
+
+    def get_image(self):
         try:
             rank = int(self.rank)
         except ValueError:
@@ -20,14 +19,16 @@ class Card:
         return "%s of %ss" % (self.rank, self.suit)
 
 class Deck:
-    def __init__(self):
+    def __init__(self, count=1):
+        self.count = count
         self.cards = self.get_cards()
 
     def get_cards(self):
         cards = []
-        for rank in range(2, 11) + ["Jack", "Queen", "King", "Ace"]:
-            for suit in ("Spade", "Heart", "Diamond", "Club"):
-                cards.append(Card(str(rank), suit))
+        for i in range(self.count):
+            for rank in range(2, 11) + ["Jack", "Queen", "King", "Ace"]:
+                for suit in ("Spade", "Heart", "Diamond", "Club"):
+                    cards.append(Card(str(rank), suit))
         random.shuffle(cards)
         return cards
 
@@ -75,45 +76,50 @@ class BlackjackHand(Hand):
         return False
 
 class Blackjack:
-    def __init__(self, root):
+    def __init__(self, root, deck, images):
         self.root = root
-        self.deck = Deck()
-        self.table = self.add_background()
+        self.deck = deck
+        self.images = images
         self.my_cards = BlackjackHand()
         self.dealer_cards = BlackjackHand()
-        self.new_game()
-        print self
         
     def new_game(self):
-        #self.my_frame.destroy()
         for i in range(2):
-            my_card = self.deck.cards.pop()
-            self.deal_card(PhotoImage(file=my_card.image))
-            self.my_cards.add(my_card)
-            dealer_card = self.deck.cards.pop()
-            self.deal_card(PhotoImage(file=dealer_card.image))
-            self.dealer_cards.add(dealer_card)
+            self.deal_card(self.my_frame, self.my_cards)
+            self.deal_card(self.dealer_frame, self.dealer_cards)
         
-    def add_background(self):
+    def add_table(self):
         self.root.title("Blackjack")
         background = PhotoImage(file="blackjack-table-layout.gif")
         self.root.geometry("%dx%d+0+0" % (background.width(), background.height()))
-        label = Label(self.root, image=background)
-        label.photo = background
-        label.pack(side='top', fill='both', expand='yes')
-        return label
+        self.table = Label(self.root, image=background)
+        self.table.photo = background
+        self.table.pack(side='top', fill='both', expand='yes')
+        self.my_frame = Frame(self.table)
+        self.my_frame.pack(side='bottom')
+        self.dealer_frame = Frame(self.table)
+        self.dealer_frame.pack(side='top')
         
-    def deal_card(self, image, **kwargs):
-        card_label = Label(self.table, image=image)
-        card_label.photo = image
-        card_label.pack(**kwargs)        
+    def deal_card(self, frame, hand):
+        card = self.deck.cards.pop()
+        image = self.images[len(self.deck.cards)]
+        label = Label(frame, image=image)
+        label.image = image
+        label.pack(side='left')
+        hand.add(card)
 
     def __str__(self):
-        s = "My Hand (%s): %s" % (str(self.my_cards.get_total()), str(self.my_cards))
-        s += "\nDealer Hand (%s): %s" % (str(self.dealer_cards.get_total()), str(self.dealer_cards))
+        s = "Dealer Hand (%s): %s" % (str(self.dealer_cards.get_total()), str(self.dealer_cards))
+        s += "\nMy Hand (%s): %s" % (str(self.my_cards.get_total()), str(self.my_cards))
         return s
 
 if __name__ == "__main__":
     root = Tk()
-    blackjack = Blackjack(root)
+    root.resizable(0, 0)
+    deck = Deck(count=2)
+    images = [PhotoImage(file=card.get_image()) for card in deck.cards]
+    blackjack = Blackjack(root, deck, images)
+    blackjack.add_table()
+    blackjack.new_game()
+    print blackjack
     root.mainloop()
