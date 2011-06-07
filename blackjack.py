559344631a529bf3e723c3a0e53a495cba7e3044
Tom Trebat <tjtrebat@gmail.com>
2011-06-07 01:00:56 -0400

    added Hand class

--- blackjack.py
+++ blackjack.py
@@ -21,6 +21,7 @@ class Deck:
         for rank in range(2, 11) + ["Jack", "Queen", "King", "Ace"]:
             for suit in ("Spade", "Heart", "Diamond", "Club"):
                 cards.append(Card(str(rank), suit))
+        random.shuffle(cards)
         return cards
 
     def __len__(self):
@@ -29,12 +30,22 @@ class Deck:
     def __str__(self):
         return ",".join([str(card) for card in self.cards])
 
+class Hand:
+    def __init__(self):
+        self.cards = []
+
+    def add(self, card):
+        self.cards.append(card)
+
+    def __str__(self):
+        return ",".join([str(card) for card in self.cards])
+
 class Blackjack:
     def __init__(self, root):
         self.root = root
         self.deck = Deck()
-        self.my_cards = []
-        self.dealer_cards = []
+        self.my_cards = Hand()
+        self.dealer_cards = Hand()
         self.add_background()
         self.new_game()
         print self
@@ -48,15 +59,13 @@ class Blackjack:
         label.pack(side='top', fill='both', expand='yes')
         
     def new_game(self):
-        self.my_cards = self.new_hand()
-        self.dealer_cards = self.new_hand()
-        
-    def new_hand(self):
-        return [self.deck.cards.pop(random.randint(0, len(self.deck))) for i in range(2)]
+        for i in range(2):
+            self.my_cards.add(self.deck.cards.pop())
+            self.dealer_cards.add(self.deck.cards.pop())
 
     def __str__(self):
-        s = "My Hand: " + ",".join([str(card) for card in self.my_cards])
-        s += "\nDealer Hand: " + ",".join([str(card) for card in self.dealer_cards])
+        s = "My Hand: " + str(self.my_cards)
+        s += "\nDealer Hand: " + str(self.dealer_cards)
         return s
 
 if __name__ == "__main__":
